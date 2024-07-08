2ec4c71c9be1cfc2f805b5a82e16b17171c9c8ea
EduardoCostadaSilva <eduardocosta5556@gmail.com>
2024-07-08 17:11:34 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from "react-native";
+import * as Notifications from 'expo-notifications';
+import { Button } from "react-native";
+import { Alert } from "react-native";
+ 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowAlert: true,
+  })
+})
+ 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ 
+  const handleCallNotifications = async () => {
+    const {status} = await Notifications.getPermissionsAsync();
+ 
+    if(status !== 'granted') {
+      Alert.alert("Você não deixou as notificações ativas");
 
+      return;
+    }
+ 
+ 
+    // Notificação Local
+    await Notifications.scheduleNotificationAsync(// para configurar a notificação dentro de um objeto.
+      {
+        content: {
+          title: "Hello world",
+          body: "Notificação do App"
+        },
+        trigger: {
+          seconds: 5,
+        }
+      }
+    )
+ 
+ 
+ 
+    /*const token = await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+    return(
+      <View>
+        <Text>{token}</Text>
+      </View>
+    )*/
+  }
+ 
+  return(
+    <View style={styles.container}>
+      <Text>App de Notificações</Text>
+      <Button title="Chamar notificação" color='red' onPress={handleCallNotifications}/>
+    </View>
+  )
+}
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+})
