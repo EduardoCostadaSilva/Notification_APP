81111b864b194c5e47483965fdcefe5f90dad07a
EduardoCostadaSilva <eduardocosta5556@gmail.com>
2024-07-10 15:03:42 -0300

    correção das permissões

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import * as Notifications from 'expo-notifications';
-import { Button } from "react-native";
-import { Alert } from "react-native";
- 
+import { Audio } from 'expo-av';
+
+const loadSound = async () => {
+  try {
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/Uêpa!(1).wav') // Verifique se o caminho e o nome do arquivo estão corretos
+    );
+    return sound;
+  } catch (error) {
+    console.error("Erro ao carregar som:", error);
+    return null;
+  }
+}
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldPlaySound: true,
@@ -10,34 +21,45 @@ Notifications.setNotificationHandler({
     shouldShowAlert: true,
   })
 })
- 
-export default function App() {
- 
-  const handleCallNotifications = async () => {
-    const {status} = await Notifications.getPermissionsAsync();
- 
-    if(status !== 'granted') {
-      Alert.alert("Você não deixou as notificações ativas");
 
+
+export default function App() {
+
+  const handleCallNotifications = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+
+
+    if (status !== 'granted') {
+      const { status: newStatus } = await Notifications.requestPermissionsAsync();
+      if (newStatus !== 'granted') {
+        Alert.alert("Permissões de notificação não concedidas");
+        return;
+      }
+    }
+
+    const sound = await loadSound();
+
+    if (!sound) {
+      Alert.alert("Erro ao carregar som");
       return;
     }
- 
- 
+
     // Notificação Local
     await Notifications.scheduleNotificationAsync(// para configurar a notificação dentro de um objeto.
       {
         content: {
-          title: "Hello world",
-          body: "Notificação do App"
+          title: "Notification",
+          body: "Crazy notification",
+          sound: sound,
         },
         trigger: {
-          seconds: 5,
+          seconds: 2,
         }
       }
     )
- 
- 
- 
+
+
+
     /*const token = await Notifications.getExpoPushTokenAsync();
     console.log(token);
     return(
@@ -46,15 +68,15 @@ export default function App() {
       </View>
     )*/
   }
- 
-  return(
+
+  return (
     <View style={styles.container}>
       <Text>App de Notificações</Text>
-      <Button title="Chamar notificação" color='red' onPress={handleCallNotifications}/>
+      <Button title="Chamar notificação" color='red' onPress={handleCallNotifications} />
     </View>
   )
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
